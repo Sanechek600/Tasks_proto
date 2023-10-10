d78ad961b519f345964ede723361072fe6fa5ee8
Sanechek600 <dassander@mail.ru>
2023-10-10 13:25:17 +0400

    A working model of Task, Profile, Comment Displayed only Task, addable only Task

--- tasks_p/tasks/admin.py
+++ tasks_p/tasks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Profile, Comment
 
 
 admin.site.register(Task)
-
+admin.site.register(Profile)
+admin.site.register(Comment)
--- tasks_p/tasks/models.py
+++ tasks_p/tasks/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+from django.db.models import OneToOneField, ManyToManyField
+
+
+class Company(models.Model):
+    pass
+
+
+class Rank(models.Model):
+    pass
+
+
+class Profile(models.Model):
+    user = OneToOneField(User, on_delete=models.CASCADE, null=True)
+    phone_number = models.CharField(max_length=12, null=True)
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
+    rank = models.ForeignKey(Rank, on_delete=models.DO_NOTHING)
+    boss = models.ForeignKey("self", on_delete=models.DO_NOTHING)
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="img/profile")
 
 
 class Task(models.Model):
@@ -7,7 +26,11 @@ class Task(models.Model):
     created = models.DateTimeField('Создана', auto_now=True)
     closed = models.DateTimeField('Закрыта', null=True, editable=False)
     changed = models.DateTimeField('Последнее изменение', auto_now=True, editable=False)
-    deadline = models.DateTimeField('Рассмотреть до') #Добавить +неделю
+    deadline = models.DateTimeField('Рассмотреть до')
+    author = models.ForeignKey('Инициатор', Profile, on_delete=models.DO_NOTHING)
+    deciders = ManyToManyField('Рассматривающие', Profile, on_delete=models.DO_NOTHING)
+    viewers = ManyToManyField('Наблюдатели', Group, on_delete=models.DO_NOTHING)
+
 
     def __str__(self):
         return self.title
@@ -15,3 +38,11 @@ class Task(models.Model):
     class Meta:
         verbose_name = 'Служебка'
         verbose_name_plural = 'Служебки'
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    published = models.DateTimeField()
+    author = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
+    task = models.ForeignKey(Task, on_delete=models.DO_NOTHING)
+
